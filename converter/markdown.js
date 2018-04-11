c677638fb1c9a04d9e6bcf126732818dde014659
rleonard <leonarduzzi@sefram.fr>
2018-04-11 10:16:32 +0200

    add parser

--- converter/markdown.js
+++ converter/markdown.js
@@ -63,6 +63,14 @@ var parseStrong = function(str) {
   return str;
  }
 
+ var parseCode = function(str) {
+ var codeRegExp = /`{1}(\w+)`{1}/;
+var stra = [];
+  while ((stra = codeRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '<pre>' + stra[1] + '</pre>');
+  }
+  return str;
+ }
 
 var markdown = {
   parse: function (str, strict) {
@@ -74,6 +82,7 @@ var markdown = {
     str = parseStrong(str);
     str = parseHorizontaleLine(str);
     str = parseLink(str);
+	str = parseCode(str);
     return str;
   }
 };
